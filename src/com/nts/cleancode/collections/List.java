995e5d801f63a7460af6e835450b16cc664887c1
Jaeyung Kim <Jaeyung Kim>
2016-06-03 20:52:02 +0900

    List.java add()method refactor

--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -10,17 +10,29 @@ public class List extends AbstractCollection {
 	}
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + 10];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
-			}
-			elements[size++] = element;
+		if (readOnly) {
+			return;
 		}
+		if (shouldGrow()) {
+			grow();
+		}
+		addElement(element);
+	}
+
+	private void grow() {
+		Object[] newElements =
+			new Object[elements.length + 10];
+		for (int i = 0; i < size; i++)
+			newElements[i] = elements[i];
+		elements = newElements;
+	}
+
+	private boolean shouldGrow() {
+		return size + 1 > elements.length;
+	}
+
+	private Object addElement(Object element) {
+		return elements[size++] = element;
 	}
 
 	public boolean contains(Object element) {
